49dc06ecbbee1a2df378839b25dacd7aa77fb5d4
shruti <shruti@finzy.com>
2020-08-01 14:28:13 +0530

    Added BuyerRepository

---
+++ src/main/java/com/shr/marketplace/repositories/BuyerRepository.java
@@ -0,0 +1,21 @@
+package com.shr.marketplace.repositories;
+
+import com.shr.marketplace.models.Buyer;
+import com.shr.marketplace.models.Seller;
+import com.shr.marketplace.repositories.mongo.CreateRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+/**
+ * Repository for Buyer entity
+ *
+ * @author shruti.mishra
+ *
+ */
+@Repository
+public interface BuyerRepository extends MongoRepository<Buyer, String> {
+
+    Optional<Buyer> findByName(String name);
+}
---
+++ src/test/java/com/shr/marketplace/repositories/BuyerRepositoryTest.java
@@ -0,0 +1,83 @@
+package com.shr.marketplace.repositories;
+
+import com.shr.marketplace.config.mongo.BaseRepositoryTest;
+import com.shr.marketplace.models.Buyer;
+import io.github.glytching.junit.extension.random.Random;
+import io.github.glytching.junit.extension.random.RandomBeansExtension;
+import org.apache.commons.lang3.reflect.FieldUtils;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Date;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ * Unit test cases
+ * for BuyerRepository
+ *
+ * @author shruti.mishra
+ */
+@SpringBootTest
+@ExtendWith(RandomBeansExtension.class)
+class BuyerRepositoryTest extends BaseRepositoryTest {
+
+    @Autowired
+    private BuyerRepository buyerRepository;
+
+    @Test
+    void shouldCreateBuyer(@Random Buyer buyer) {
+
+        final var savedBuyer = buyerRepository.save(buyer);
+
+        assertNotNull(savedBuyer.getId());
+        assertNotNull(buyerRepository.findById(savedBuyer.getId()));
+    }
+
+    @Test
+    void shouldGetBuyerById(@Random Buyer buyer) {
+
+        final var savedBuyer = buyerRepository.save(buyer);
+        assertNotNull(savedBuyer.getId());
+        assertNotNull(buyerRepository.findById(savedBuyer.getId()));
+    }
+
+    @Test
+    void shouldGetBuyerByName() {
+
+        final var buyerName = "Buyer's name";
+        final var buyer = new Buyer(buyerName);
+
+        final var savedBuyer = buyerRepository.save(buyer);
+        assertNotNull(savedBuyer.getId());
+        assertNotNull(buyerRepository.findByName(savedBuyer.getName()));
+    }
+
+    @Test
+    void shouldUpdateBuyer() throws IllegalAccessException {
+
+        final var buyer = new Buyer("Buyer's name");
+        final var savedBuyer = buyerRepository.save(buyer);
+
+        assertNotNull(savedBuyer.getId());
+        assertThat(savedBuyer.getName(), is(buyer.getName()));
+
+        final var buyersNewName = "Buyer's new name";
+
+        Date oldDate = new Date(2020, 02, 21);
+        savedBuyer.setCreatedAt(oldDate);
+        FieldUtils.getField(Buyer.class, "name", true)
+                .set(savedBuyer, buyersNewName);
+
+
+        buyerRepository.save(savedBuyer);
+
+        final var retrievedBuyer = buyerRepository.findById(savedBuyer.getId()).orElseThrow();;
+        assertThat(retrievedBuyer.getName(), is(buyersNewName));
+        assertThat(retrievedBuyer.getCreatedAt(), is(oldDate));
+    }
+}
