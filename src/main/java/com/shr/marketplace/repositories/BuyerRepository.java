1b27fe2901d1456ce2c5f74d5c324edcde9cc43d
shruti <shruti@finzy.com>
2020-08-01 14:30:50 +0530

    Removed unused imports

--- src/main/java/com/shr/marketplace/repositories/BuyerRepository.java
+++ src/main/java/com/shr/marketplace/repositories/BuyerRepository.java
@@ -1,8 +1,6 @@
 package com.shr.marketplace.repositories;
 
 import com.shr.marketplace.models.Buyer;
-import com.shr.marketplace.models.Seller;
-import com.shr.marketplace.repositories.mongo.CreateRepository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
