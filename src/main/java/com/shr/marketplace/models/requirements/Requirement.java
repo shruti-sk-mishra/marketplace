b7852ae9490c454d0802f37b741a2ff63fbbde2b
shruti <shruti@finzy.com>
2020-08-02 15:33:30 +0530

    Corrected the class level comments

--- src/main/java/com/shr/marketplace/models/requirements/Requirement.java
+++ src/main/java/com/shr/marketplace/models/requirements/Requirement.java
@@ -3,7 +3,7 @@ package com.shr.marketplace.models.requirements;
 import com.shr.marketplace.models.BaseModel;
 
 /**
- *
+ * Base class for any project requirement
  * @author shruti.mishra
  */
 public class Requirement extends BaseModel {
--- src/main/java/com/shr/marketplace/repositories/ProjectRepository.java
+++ src/main/java/com/shr/marketplace/repositories/ProjectRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 /**
- * Repository for Buyer entity
+ * Repository for Project entity
  *
  * @author shruti.mishra
  *
