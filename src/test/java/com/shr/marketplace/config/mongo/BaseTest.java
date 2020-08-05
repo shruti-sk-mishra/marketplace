2cd88dd9d3830ffa6bb26e6d327eb20cdbfe3a21
shruti <shruti@finzy.com>
2020-08-06 01:22:59 +0530

    Included 'application-test.properties' to be used for getting the testing environment

--- src/test/java/com/shr/marketplace/config/mongo/BaseTest.java
+++ src/test/java/com/shr/marketplace/config/mongo/BaseTest.java
@@ -1,6 +1,8 @@
 package com.shr.marketplace.config.mongo;
 
 import org.jeasy.random.EasyRandom;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 
 /**
@@ -8,6 +10,8 @@ import org.jeasy.random.EasyRandom;
  * each test class
  *
  */
+@TestPropertySource(locations = "classpath:application-test.properties")
+@ActiveProfiles("test")
 public class BaseTest {
     private final EasyRandom easyRandom = new EasyRandom();
 
