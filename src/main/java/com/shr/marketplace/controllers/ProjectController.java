29706d6cd09bc3431e92881e625c4bc692e3f1ca
shruti <shruti@finzy.com>
2020-08-06 10:48:19 +0530

    Added ProjectController

---
+++ src/main/java/com/shr/marketplace/controllers/ProjectController.java
@@ -0,0 +1,45 @@
+package com.shr.marketplace.controllers;
+
+import com.shr.marketplace.models.Project;
+import com.shr.marketplace.services.ProjectService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+/**
+ * @author shruti.mishra
+ */
+@RestController
+@RequestMapping("/project")
+public class ProjectController {
+    private final ProjectService projectService;
+
+    private final Logger logger = LoggerFactory.getLogger(ProjectController.class);
+
+    public ProjectController(ProjectService projectService) {
+            this.projectService = projectService;
+        }
+
+
+    @PostMapping
+    public ResponseEntity<Project> create(@Valid @RequestBody Project project) {
+
+        //logger.info("Attempting to create the project={}", project);
+        final var createdProject = projectService.create(project);
+        //logger.info("Project created={}", createdProject);
+        return new ResponseEntity<>(createdProject, HttpStatus.CREATED);
+    }
+
+    @PutMapping
+    public ResponseEntity<Void> update(@Valid @RequestBody Project project) {
+
+        logger.info("Updating the project={}", project);
+        final var createdProject = projectService.update(project);
+        logger.info("Project updated={}", createdProject);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
