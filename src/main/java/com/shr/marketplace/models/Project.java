dcfd0d7161d4837d3e6488f7a0028570ba80451b
shruti <shruti@finzy.com>
2020-08-06 22:31:16 +0530

    Changed the default constructor access modifiers from private to public

--- src/main/java/com/shr/marketplace/models/Project.java
+++ src/main/java/com/shr/marketplace/models/Project.java
@@ -46,7 +46,7 @@ public class Project extends BaseDocument {
     private Status status;
 
     // Required for object creation by ObjectMapper
-    private Project() {}
+    public Project() {}
 
     public Project(@NotBlank String name, @NotBlank ProjectType type,
                    @NotBlank String description) {
