19cc5dcc809b161b31f3a44a4ba836f42f49c8ff
shruti <shruti@finzy.com>
2020-08-08 21:02:29 +0530

    Enabled caching in MarketplaceApplication

--- src/main/java/com/shr/marketplace/MarketplaceApplication.java
+++ src/main/java/com/shr/marketplace/MarketplaceApplication.java
@@ -3,7 +3,9 @@ package com.shr.marketplace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @EnableConfigurationProperties
 @SpringBootApplication(scanBasePackages = "com.shr")
 public class MarketplaceApplication {
